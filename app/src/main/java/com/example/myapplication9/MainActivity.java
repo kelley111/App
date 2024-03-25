5bfc098aeadd5205a2f6bb79521ed5632f1b48c1
Kathy <1509919037@qq.com>
2024-03-25 20:52:12 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication9/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.myapplication9;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    int m=0;
+    public void onClick3(View v){
+        m= m +3;
+        TextView show1 =findViewById(R.id.txtout1);
+        show1.setText(String.valueOf(m));
+
+    }
+    public void onClick2(View v){
+        m= m +2;
+        TextView show1 =findViewById(R.id.txtout1);
+        show1.setText(String.valueOf(m));
+
+    }
+    public void onClick1(View v){
+        m= m +1;
+        TextView show1 =findViewById(R.id.txtout1);
+        show1.setText(String.valueOf(m));
+
+    }
+
+    public void onClick0(View v){
+        m=0;
+        TextView show1 =findViewById(R.id.txtout1);
+        show1.setText(String.valueOf(m));
+
+    }
+    public void onClick4(View v){
+        m= m -1;
+        TextView show1 =findViewById(R.id.txtout1);
+        show1.setText(String.valueOf(m));
+
+    }
+    public void onClick5(View v){
+        m= m -2;
+        TextView show1 =findViewById(R.id.txtout1);
+        show1.setText(String.valueOf(m));
+
+    }
+    public void onClick6(View v){
+        m= m -3;
+        TextView show1 =findViewById(R.id.txtout1);
+        show1.setText(String.valueOf(m));
+
+    }
+
+
+
+}
